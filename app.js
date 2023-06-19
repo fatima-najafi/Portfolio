aea753b7f05bfa37222bda58cf7d5e18f95b0352
Fatima Najafi <najafifatima762@gmail.com>
2023-06-19 19:03:12 +0430

    add update js code in app.js file

--- app.js
+++ app.js
@@ -3,14 +3,14 @@ const hideNav = document.getElementById("hide-nav");
 const mobileMenuLinks = document.getElementById("mobile-menu-links");
 const mobileMenu = document.getElementById("mobile-menu");
 
-// displayNav.addEventListener("click", () => {
-//   mobileMenu.classList.toggle("mobile-menu--show");
-// });
+displayNav.addEventListener("click", () => {
+  mobileMenu.classList.toggle("mobile-menu--show");
+});
 
-// hideNav.addEventListener("click", () => {
-//   mobileMenu.classList.toggle("mobile-menu--show");
-// });
+hideNav.addEventListener("click", () => {
+  mobileMenu.classList.toggle("mobile-menu--show");
+});
 
-// mobileMenuLinks.addEventListener("click", () => {
-//   mobileMenu.classList.toggle("mobile-menu--show");
-// });
+mobileMenuLinks.addEventListener("click", () => {
+  mobileMenu.classList.toggle("mobile-menu--show");
+});
