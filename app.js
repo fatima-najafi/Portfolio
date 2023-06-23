633a1f0493e9c2ef7485e8e225855e730b648ef1
Fatima Najafi <najafifatima762@gmail.com>
2023-06-23 11:28:04 +0430

    fix javascript file errors

--- app.js
+++ app.js
@@ -1,18 +1,18 @@
-const showNav = document.getElementById("show-nav-btn");
-const hideNav = document.getElementById("hide-nav-btn");
-const mobileMenuLinks = document.getElementById("mobile-menu-links");
-const mobileMenu = document.getElementById("mobile-menu");
+const showNav = document.getElementById('show-nav-btn');
+const hideNav = document.getElementById('hide-nav-btn');
+const mobileMenuLinks = document.getElementById('mobile-menu-links');
+const mobileMenu = document.getElementById('mobile-menu');
 
-showNav.addEventListener("click", () => {
-  mobileMenu.classList.toggle("mobile-menu--show");
+showNav.addEventListener('click', () => {
+  mobileMenu.classList.toggle('mobile-menu--show');
 });
 
-hideNav.addEventListener("click", () => {
-  mobileMenu.classList.toggle("mobile-menu--show");
+hideNav.addEventListener('click', () => {
+  mobileMenu.classList.toggle('mobile-menu--show');
 });
 
-mobileMenuLinks.addEventListener("click", () => {
-  mobileMenu.classList.toggle("mobile-menu--show");
+mobileMenuLinks.addEventListener('click', () => {
+  mobileMenu.classList.toggle('mobile-menu--show');
 });
 
 // mobileMenuLinks.forEach((link) => {
